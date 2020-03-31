d8a6ca894fe31e9f5dad3c5af4733c1f4cc77fe0
bakdoolotkeldibekova <54860550+bakdoolotkeldibekova@users.noreply.github.com>
2020-03-31 10:22:41 +0600

    Create Scoring.java

---
+++ 58 - Threads 2 [Задание] Футбольный матч/Scoring.java
@@ -0,0 +1,9 @@
+public class Scoring {
+    public static int goalTeam1=0;
+    public static int yellowCardTeam1=0;
+    public static int offsideTeam1=0;
+
+    public static int goalTeam2=0;
+    public static int yellowCardTeam2=0;
+    public static int offsideTeam2=0;
+}
